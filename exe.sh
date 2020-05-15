d1f2169e4026c6b4699908707c6109bda60efeee
Abhishek Kumar <abhsihek_ku@yahoo.com>
2020-05-15 21:03:02 +0530

    Updated

---
+++ exe.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker exec  -it aktechthoughts/airflow_web:latest /bin/bash
--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 docker run  --name=airflow_web -d \
-	-v /home/jenkins:/var/jenkins_home \
-	-p 8080:8080 \
-		aktechthoughts/aws-jenkins-docker:7 \
+    -e AIRFLOW__CORE__SQL_ALCHEMY_CONN=sqlite:////usr/local/airflow/db/airflow.db \
+	-v /home/abhishek/airflow/dags:/usr/local/airflow/dags \
+	-v /home/abhishek/airflow/db:/usr/local/airflow/db \
+	-p 8888:8888 \
 	--log-opt max-size=10m \
-	--log-opt max-file=5
-
+	--log-opt max-file=5 \
+		aktechthoughts/airflow_web:latest \
+		webserver -p 8888
 
