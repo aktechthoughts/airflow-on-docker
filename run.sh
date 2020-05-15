3214c7b19904879e183899934ac181aba5647a1c
Abhishek Kumar <abhsihek_ku@yahoo.com>
2020-05-15 22:29:44 +0530

    Updated

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 docker run  --name=airflow_web -d \
-    -e AIRFLOW__CORE__SQL_ALCHEMY_CONN=sqlite:////usr/local/airflow/db/airflow.db \
-	-v /home/abhishek/airflow/dags:/usr/local/airflow/dags \
-	-v /home/abhishek/airflow/db:/usr/local/airflow/db \
+	-v /home/ubuntu/airflow/dags:/usr/local/airflow/dags \
+	-v /home/ubuntu/airflow/db:/usr/local/airflow/db \
 	-p 8888:8888 \
 	--log-opt max-size=10m \
 	--log-opt max-file=5 \
-		aktechthoughts/airflow_web:latest 
+		aktechthoughts/airflow_web:1 
 
 
