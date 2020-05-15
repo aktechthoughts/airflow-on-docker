39e6b1615a7b85ed22e7f0cc8e962134059eaa9e
Add to docker group <ubuntu@ip-10-0-2-240.eu-central-1.compute.internal>
2020-05-15 17:59:51 +0000

    Working

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ docker run --name airflow_web \
       -p 8888:8888 \
       -v /home/ubuntu/airflow/dags:/usr/local/airflow/dags \
       -v /home/ubuntu/airflow/db:/usr/local/airflow/db \
-      -d -t aktechthoughts/airflow_web:1
+      -d -t aktechthoughts/airflow_web:latest
 
 
 
